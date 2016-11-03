c23d8d9c4446e746b687919b631b404909ffeb59
C CA <C CA>
2016-11-03 16:53:44 +0800

    Make changes to manage cases, able to aaprove and reject cases

---
+++ controllers/cases_manage.js
@@ -0,0 +1,223 @@
+var uid;
+var entries = new Array();
+
+$(document).ready(function() {
+    setTimeout(function () {
+        console.log("i am in set timeout");
+        $('#TABLE').DataTable( {
+            data: entries,
+            columns: [
+                { title: "#" },
+                { title: "Latitude" },
+                { title: "Longitute" },
+                { title: "Types" },
+                { title: "Remarks" },
+                { title: "Status" },
+                { title: "Cases ID" },
+                { title: "Actions" }
+            ]
+        } );
+    },2500);
+        
+        console.log(entries.length + " entries length");
+
+        
+} );
+
+$('#TABLE').on('draw.dt', function() {
+            if(entries.length >0){
+                 console.log("i am in page change");
+        var table = document.getElementById('TABLE');
+        for (var r = 1, n = table.rows.length; r < n; r++) {
+            for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+                if(c==(table.rows[r].cells.length-1)){   
+                table.rows[r].cells[c].innerHTML = '<div class="btn-group"><button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false"> Actions <i class="fa fa-angle-down"></i></button>'+
+                                '<ul class="dropdown-menu" role="menu"><li><a href="javascript:;"><a onclick="clicked2(this.parentNode.parentNode.parentNode.parentNode.parentNode);"><i onclick="clicked2(this);"<i class="icon-docs"></i> Approve </a></li><li><a onclick="clicked(this.parentNode.parentNode.parentNode.parentNode.parentNode);"><i onclick="clicked2(this);" class="icon-tag"></i> Cancel Cases </a></li></ul></div>';
+                }
+            }
+        }
+            }
+        });
+
+    var count25 = 0;
+ var table3 = firebase.database().ref('/DengueCases/UserCases');
+                     
+                        table3.on('value', function(snapshot) {
+
+                        snapshot.forEach(function() {
+                        count25++;
+                    });
+                     });
+
+
+    function clicked(evt) {
+            var tbody = document.getElementById('TBODY');
+            var casesid;
+            casesid = evt.cells[6].innerHTML;
+               console.log(casesid);
+            //sessionStorage.setItem('bookingid', bookingid);
+          //  window.location = "appointment_cancel.html"
+                    var db2 = firebase.database();
+                    var cases2Ref = db2.ref("Cases");
+                     cases2Ref.child(uid).child(casesid).update({status: "Cancelled"});
+                     alert("The case has been cancelled!");
+                     window.location.reload();
+                     
+
+
+        }
+
+ function clicked2(evt) {
+
+
+            var tbody = document.getElementById('TBODY');
+            var count = 0;
+            var casesid;
+            casesid = evt.cells[6].innerHTML;
+            var lat;
+            lat = evt.cells[1].innerHTML;
+            var long;
+            long = evt.cells[2].innerHTML;
+               console.log(casesid);
+            //sessionStorage.setItem('bookingid', bookingid);
+          //  window.location = "appointment_cancel.html"
+                    var db2 = firebase.database();
+                    var dengues2Ref = db2.ref("DengueCases");
+                     var cases2Ref = db2.ref("Cases");
+                     cases2Ref.child(uid).child(casesid).update({status: "Approved"});
+                     alert("The case has been Approved!");
+                   
+
+                    
+
+
+                   firebase.database().ref('/DengueCases/UserCases/' + (count25+1)).set({
+        
+                    lat: lat,
+                     long: long
+                   });
+
+                     window.location.reload();
+  //});
+
+
+  /*var newPostKey = firebase.database().ref().child('DengueCases').push().key;
+   var updates = {};
+  updates['/DengueCases/UserCases' + count + '/' + newPostKey] = postData;
+
+ 
+
+  return firebase.database().ref().update(updates);*/
+
+
+
+
+
+        }
+
+
+
+
+
+
+    function getCases()
+    {
+
+            console.log(uid);
+            var db = firebase.database();
+            var CasesRef = db.ref("Cases");
+             //To store the appointment entries
+            var count = 0; //To keep track of the array and also for index
+
+            var myTableDiv = document.getElementById("container");
+            var table = document.getElementById('TABLE');
+            var tableBody = document.getElementById('TBODY');
+            var tableHead = document.getElementById('THEAD');
+            console.log("i am in getCases after table declaration")
+            
+            table.border = '1'
+            table.appendChild(tableBody);
+
+            var tr = document.createElement('tr');
+
+            var ref3 = CasesRef.orderByKey().equalTo(uid).on("value", function(snapshot) {
+                console.log(snapshot.numChildren());
+                console.log(snapshot.val());
+                snapshot.forEach(function(data) {
+                    data.forEach(function(childData) {
+                        //childData.val().status!="Cancelled"
+
+                        if(childData.val().status=="Pending"){
+                        entries[count] = new Array(count + 1, childData.val().coordinaresX, childData.val().coordinaresY, childData.val().remarks, childData.val().status, childData.val().type,childData.getKey(),"");
+                        count++;
+                    }
+                    });
+                    for (i = 0; i < entries.length; i++) {
+                        var tr = document.createElement('tr');
+                        
+                        tr.id = "row" + i;
+                        //tr.addEventListener("click", clicked);
+                        //mouseclic.addEventListener("click", clicked);
+
+                        for (j = 0; j < entries[i].length; j++) {
+                            var td = document.createElement('td');
+                            td.appendChild(document.createTextNode(entries[i][j]));
+                            tr.appendChild(td);
+                            //if(j==entries[i].length-1){   
+                                //console.log("I am in innerHTML");
+  
+                                 //td.innerHTML = '<div class="btn-group"><button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false"> Actions <i class="fa fa-angle-down"></i></button>'+
+                                 //                       '<ul class="dropdown-menu" role="menu"><li><a href="javascript:;"><i class="icon-docs"></i> Edit </a></li><li><a onclick="clicked(this.parentNode.parentNode.parentNode.parentNode.parentNode);"><i onclick="clicked(this);" class="icon-tag"></i> Cancel Appointment </a></li></ul></div>';
+                                
+                               // tr.appendChild(document.createTextNode("hello"));
+                            //}
+                        }
+                        tableBody.appendChild(tr);
+                    }
+                    myTableDiv.appendChild(table)
+                });
+
+
+                console.log(entries.length + "entries length");
+
+            });
+
+
+
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+      function initApp() {
+            var logoutbtn = document.getElementById('logoutbtn');
+            
+            firebase.auth().onAuthStateChanged(function(user) {
+  
+                if (user) {
+                    uid = user.uid;
+                    console.log("I am in initApp")
+                    getCases();
+                    logoutbtn.addEventListener('click', function() {
+                        firebase.auth().signOut();
+                        window.location = "page_user_login_1.html"
+                    });
+
+                } else {
+                    window.location = "page_user_login_1.html";
+                }
+            });
+        }
+
+        window.onload = function() {
+            initApp();
+        };
